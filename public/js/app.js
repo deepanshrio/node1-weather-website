c91d740b4899ba35b713ebfdbf730c2efece7fcf
Deepansh Pandiyar <deepa@Deepansh>
2023-06-14 13:04:07 +0530

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Javascript file is loaded !')
+//console.log('Javascript file is loaded !')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //       response.json().then((data) => {
